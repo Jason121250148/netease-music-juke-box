509a0cf27d3cc22e83d24b73c9808b595c37ed69
Jason121250148 <962215093@qq.com>
2016-07-14 14:16:21 +0800

    Add onselectionEvent.

--- src/nju/view/ListView.js
+++ src/nju/view/ListView.js
@@ -103,6 +103,8 @@ export default class ListView extends View
             const $item = this.$getItem(item);
             $item.addClass("selected");
         }
+
+        this.trigger("selectionChanged");
     }
 
     getIdOfItem(item)
